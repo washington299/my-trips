ee6bc08dbb68ed3cb0c6f7b47e7201e7d5252ed8
Washington Campos <washingtoncampos9@gmail.com>
2021-07-10 18:39:47 -0300

    feat: create first leaflet Map component

--- src/components/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-const Main = () => {
-	return (
-		<main>
-			<h1>My trips</h1>
-		</main>
-	);
-};
-
-export default Main;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ const App = ({ Component, pageProps }: AppProps) => {
 				<title>My trips</title>
 				<link rel="shortcut icon" href="/img/favicon.png" />
 				<link rel="apple-touch icon" href="/img/favicon.png" />
+				<link
+					rel="stylesheet"
+					href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+					integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+					crossOrigin=""
+				/>
 				<meta
 					name="description"
 					content="A project that shows the places i've visited in my whole life"
