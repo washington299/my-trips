81bbb096bfde0211650e41cb32fca3b151b969c9
Washington Campos <washingtoncampos9@gmail.com>
2021-07-11 19:57:12 -0300

    feat: create Graphql getPages query

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,16 @@
+import client from "graphql/client";
+import { GET_PAGES } from "graphql/queries";
+
 import AboutTemplate from "templates/About";
 
-const About = () => <AboutTemplate />;
+const AboutPage = () => <AboutTemplate />;
 
-export default About;
+export const getStaticProps = async () => {
+	await client.request(GET_PAGES);
+
+	return {
+		props: {},
+	};
+};
+
+export default AboutPage;
