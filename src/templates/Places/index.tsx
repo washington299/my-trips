35d98b02cb5e68f9a0ce4758b0cc6bb1e2c4b0eb
Washington Campos <washingtoncampos9@gmail.com>
2021-07-28 13:17:32 -0300

    feat: add styles to place page

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,9 @@
+import { CloseOutline } from "@styled-icons/evaicons-outline/CloseOutline";
+
+import LinkWrapper from "components/LinkWrapper";
+
+import * as S from "templates/Places/styles";
+
 type ImageProps = {
 	url: string;
 	width: number;
@@ -18,15 +24,22 @@ export type PlacesTemplateProps = {
 
 const PlacesTemplate = ({ place }: PlacesTemplateProps) => {
 	return (
-		<>
-			<h1>{place.name}</h1>
+		<S.Wrapper>
+			<LinkWrapper href="/">
+				<CloseOutline size={32} aria-label="HomeGo back to map" />
+			</LinkWrapper>
+			<S.Container>
+				<S.Heading>{place.name}</S.Heading>
 
-			<div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+				<S.Body dangerouslySetInnerHTML={{ __html: place.description.html }} />
 
-			{place.gallery.map(({ url }) => (
-				<img key={`Photo-${place.id}`} src={url} alt={place.name} />
-			))}
-		</>
+				<S.Gallery>
+					{place.gallery.map(({ url }) => (
+						<img key={`Photo-${url}`} src={url} alt={place.name} width="100%" height={500} />
+					))}
+				</S.Gallery>
+			</S.Container>
+		</S.Wrapper>
 	);
 };
 
