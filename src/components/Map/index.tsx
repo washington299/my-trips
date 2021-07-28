618677dd5d2f20e0073b119268f63c855c146453
Washington Campos <washingtoncampos9@gmail.com>
2021-07-28 12:59:42 -0300

    feat: redirect user to place page when clicked on marker

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 type Place = {
@@ -15,17 +16,24 @@ export type MapProps = {
 };
 
 const Map = ({ places }: MapProps) => {
+	const router = useRouter();
+
 	return (
 		<MapContainer center={[51.505, -0.09]} zoom={3} style={{ width: "100%", height: "100vh" }}>
 			<TileLayer
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			{places?.map(({ id, name, location }) => {
+			{places?.map(({ id, name, slug, location }) => {
 				const { latitude, longitude } = location;
 
 				return (
-					<Marker key={id} position={[latitude, longitude]} title={name}>
+					<Marker
+						key={id}
+						position={[latitude, longitude]}
+						title={name}
+						eventHandlers={{ click: () => router.push(`/place/${slug}`) }}
+					>
 						<Popup>{name}</Popup>
 					</Marker>
 				);
